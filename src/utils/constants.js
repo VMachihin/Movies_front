6353acbae5e4b53461edb039875d3cb9c32cc647
VitMach <machihin_vitalii@mail.ru>
2024-08-13 01:47:31 +0300

    fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'https://api.movies.lib.nomoredomains.rocks.vitmach.ru';
-const MOVIES_URL = 'https://api.nomoreparties.co';
+const BASE_URL = "api.movies.vitmach.ru";
+const MOVIES_URL = "https://api.nomoreparties.co";
 const SHORT_MOVIE_DURATION = 40;
 
 const laptopWidth = 1024;
@@ -13,15 +13,15 @@ const numberCardsToShowMobile = 5;
 const quantityLoadingMoviesLaptop = 3;
 const quantityLoadingMoviesTablet = 2;
 export {
-  BASE_URL,
-  MOVIES_URL,
-  SHORT_MOVIE_DURATION,
-  laptopWidth,
-  tabletWidth,
-  mobileWidth,
-  numberCardsToShowLaptop,
-  numberCardsToShowTablet,
-  numberCardsToShowMobile,
-  quantityLoadingMoviesLaptop,
-  quantityLoadingMoviesTablet,
+	BASE_URL,
+	MOVIES_URL,
+	SHORT_MOVIE_DURATION,
+	laptopWidth,
+	tabletWidth,
+	mobileWidth,
+	numberCardsToShowLaptop,
+	numberCardsToShowTablet,
+	numberCardsToShowMobile,
+	quantityLoadingMoviesLaptop,
+	quantityLoadingMoviesTablet,
 };
